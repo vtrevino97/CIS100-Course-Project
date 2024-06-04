8e79d5c07a2e6ec3c1fd8d311ec161b876eea92e
username <useremail>
2024-06-03 20:04:01 -0700

    Project

--- CIS100_Course_Project.py
+++ CIS100_Course_Project.py
@@ -1,1 +1,11 @@
+print(f"Hello, everyone! I have a hilarious story to tell you about my pet rabbit. I'm excited to tell you all about it.")
+print(f"Before I tell you the story, I have a few questions I would like to ask you.")
+print(f"After typing your answer, please don't forget to press the enter key.")
+input(f"\nPress the enter key to continue on.")
 
+#5 Questions before the story begins
+petType = input(f"\nWhat type of pet is it:  ")
+petName = input(f"What is the pet's name:  ")
+parkName = input(f"What park are you playing at:  ")
+time = input(f"What time is it:  ")
+foodType = input(f"What is your favorite food:  ")
